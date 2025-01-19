266acfb67b1611b756f631ef45c6fae32ee22cc3
OskarSko <skowronskioskar372@gmail.com>
2025-01-19 18:07:22 +0100

    lab12-2

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import styles from './page.module.css';
 import Link from 'next/link';
-
+import ImageSlideshow from './components/images/image-slideshow';
 export default function Home() {
   return (
     <>
     <header className={styles.header}>
       <div className={styles.slideshow}>
-
+        <ImageSlideshow />
       </div>
       <div>
       <div className={styles.hero}>
@@ -34,6 +34,11 @@ export default function Home() {
       </section>
       <section className={styles.section}>
         <h2>Why NextLevel Food?</h2>
+        <p>
+          NextLevel Food is a platform for foodies to share their favorite
+          recipes with the world. It&apos;s a place to discover new dishes, and to
+          connect with other food lovers.
+        </p>
         <p>
           NextLevel Food is a place to discover new dishes, and to connect
           with other food lovers.
