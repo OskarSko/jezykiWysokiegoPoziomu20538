caa1b7be4d8fd9f5bd53f945266a85489d7c7708
OskarSko <skowronskioskar372@gmail.com>
2025-01-19 17:18:53 +0100

    lab11-1

--- app/meals/[someName]/page.js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/navigation';
-
-export default function MealDetailsPage({ params }) {
-  const { someName } = params;
-
-  return <h1>Meal Details for {someName}</h1>;
-}
