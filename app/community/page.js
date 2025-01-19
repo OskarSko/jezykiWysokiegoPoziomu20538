f128affe2512fe7eebd7863d1d2ce5fea2b05f0b
OskarSko <skowronskioskar372@gmail.com>
2025-01-19 17:16:12 +0100

    lab11-1

---
+++ app/community/page.js
@@ -0,0 +1,4 @@
+export default function CommunityPage() {
+    return <h1>Welcome to the Foodies Community</h1>;
+  }
+  
---
+++ app/meals/share/page.js
@@ -0,0 +1,4 @@
+export default function SharePage() {
+    return <h1>Share Your Favorite Meals</h1>;
+  }
+  
