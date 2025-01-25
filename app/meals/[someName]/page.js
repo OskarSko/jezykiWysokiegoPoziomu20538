b89be7aa9c8cd678196db195cd5a31ec3f0753ab
OskarSko <skowronskioskar372@gmail.com>
2025-01-25 18:47:20 +0100

    lab13-8

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -4,7 +4,6 @@ import classes from './page.module.css';
 
 export default async function MealDetailsPage({ params }) {
   const { someName } = await params;
-  const meal = getMeal(someName);
   return(
     <main>
         <h1>Meal details for {someName}</h1>
