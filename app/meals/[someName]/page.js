c89b64dc439689871aba92665181379ff2167257
OskarSko <skowronskioskar372@gmail.com>
2025-01-25 18:41:17 +0100

    lab13-5

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -5,13 +5,6 @@ export default async function MealDetailsPage({ params }) {
   const { someName } = await params;
   const meal = getMeal(someName);
 
-  if (!meal) {
-    return (
-      <main>
-        <div className={classes.loading}>Loading meal details...</div>
-      </main>
-    );
-  }
 
   return (
     <h2>Instructions</h2>
